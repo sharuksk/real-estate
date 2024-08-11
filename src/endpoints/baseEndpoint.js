b8e298df837e9924f811202056117fb77c59553e
AravindKumar40m <aravindkumar40m@gmail.com>
2024-08-11 09:49:51 +0530

    changed based endpoint to hosted backend endpoint in client

--- src/endpoints/baseEndpoint.js
+++ src/endpoints/baseEndpoint.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:8000/api/v1";
+// export const BASE_URL = "http://localhost:8000/api/v1";
+export const BASE_URL = "https://real-estate-backend-lny9.onrender.com";
